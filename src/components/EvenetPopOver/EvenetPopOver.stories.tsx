4729dbf9dbc5fa7933b3d340488302954e94e773
Masud Rana <masudrana2779@gmail.com>
2024-02-25 16:46:31 +0600

    added

--- src/components/EvenetPopOver/EvenetPopOver.stories.tsx
+++ src/components/EvenetPopOver/EvenetPopOver.stories.tsx
@@ -1,19 +1,19 @@
-import { JGSBGROUP, createSBItem, createSBTpl } from '@/sb/JGStoryBook'
-import EvenetPopOver from './EvenetPopOver'
+// import { JGSBGROUP, createSBItem, createSBTpl } from '@/sb/JGStoryBook'
+// import EvenetPopOver from './EvenetPopOver'
 
-export default createSBItem(EvenetPopOver, {
-  section: JGSBGROUP.EVENTMANAGEMENT,
-  title: 'Evenet PopOver',
-})
+// export default createSBItem(EvenetPopOver, {
+//   section: JGSBGROUP.EVENTMANAGEMENT,
+//   title: 'Evenet PopOver',
+// })
 
-const tpl = createSBTpl<typeof EvenetPopOver>((args) => {
-  return <EvenetPopOver {...args} />
-})
+// const tpl = createSBTpl<typeof EvenetPopOver>((args) => {
+//   return <EvenetPopOver {...args} />
+// })
 
-export const Basic = tpl.bind({})
-Basic.args = {
-  startTime: '10:00 AM',
-  endTime: '11:00 AM',
-  date: 'Monday, 19 February 2024',
-  sellHeight: '100px',
-}
+// export const Basic = tpl.bind({})
+// Basic.args = {
+//   startTime: '10:00 AM',
+//   endTime: '11:00 AM',
+//   date: 'Monday, 19 February 2024',
+//   sellHeight: '100px',
+// }
--- src/components/ScheduleListView/ScheduleListView.stories.tsx
+++ src/components/ScheduleListView/ScheduleListView.stories.tsx
@@ -1,93 +1,93 @@
-import { JGSBGROUP, createSBItem, createSBTpl } from '@/sb/JGStoryBook'
-import ScheduleListView from './ScheduleListView'
+// import { JGSBGROUP, createSBItem, createSBTpl } from '@/sb/JGStoryBook'
+// import ScheduleListView from './ScheduleListView'
 
-export default createSBItem(ScheduleListView, {
-  section: JGSBGROUP.EVENTMANAGEMENT,
-  title: 'Schedule List View',
-})
+// export default createSBItem(ScheduleListView, {
+//   section: JGSBGROUP.EVENTMANAGEMENT,
+//   title: 'Schedule List View',
+// })
 
-const tpl = createSBTpl<typeof ScheduleListView>((args) => {
-  return <ScheduleListView {...args} />
-})
+// const tpl = createSBTpl<typeof ScheduleListView>((args) => {
+//   return <ScheduleListView {...args} />
+// })
 
-export const Basic = tpl.bind({})
-Basic.args = {
-  month: 'February 2024',
-  eventsDays: [
-    {
-      eventDate: '19',
-      eventDay: 'MON',
-      events: [
-        {
-          startTime: '10:00 AM',
-          endTime: '11:00 AM',
-          date: 'Monday, 19 February 2024',
-        },
-      ],
-    },
-    {
-      eventDate: '20',
-      eventDay: 'MON',
-      events: [
-        {
-          startTime: '10:00 AM',
-          endTime: '11:00 AM',
-          date: 'Monday, 19 February 2024',
-        },
-        {
-          startTime: '10:00 AM',
-          endTime: '11:00 AM',
-          date: 'Monday, 19 February 2024',
-        },
-        {
-          startTime: '10:00 AM',
-          endTime: '11:00 AM',
-          date: 'Monday, 19 February 2024',
-        },
-        {
-          startTime: '10:00 AM',
-          endTime: '11:00 AM',
-          date: 'Monday, 19 February 2024',
-        },
-        {
-          startTime: '10:00 AM',
-          endTime: '11:00 AM',
-          date: 'Monday, 19 February 2024',
-        },
-      ],
-    },
-    {
-      eventDate: '21',
-      eventDay: 'MON',
-      events: [
-        {
-          startTime: '10:00 AM',
-          endTime: '11:00 AM',
-          date: 'Monday, 19 February 2024',
-        },
-      ],
-    },
-    {
-      eventDate: '22',
-      eventDay: 'MON',
-      events: [
-        {
-          startTime: '10:00 AM',
-          endTime: '11:00 AM',
-          date: 'Monday, 19 February 2024',
-        },
-      ],
-    },
-    {
-      eventDate: '23',
-      eventDay: 'MON',
-      events: [
-        {
-          startTime: '10:00 AM',
-          endTime: '11:00 AM',
-          date: 'Monday, 19 February 2024',
-        },
-      ],
-    },
-  ],
-}
+// export const Basic = tpl.bind({})
+// Basic.args = {
+//   month: 'February 2024',
+//   eventsDays: [
+//     {
+//       eventDate: '19',
+//       eventDay: 'MON',
+//       events: [
+//         {
+//           startTime: '10:00 AM',
+//           endTime: '11:00 AM',
+//           date: 'Monday, 19 February 2024',
+//         },
+//       ],
+//     },
+//     {
+//       eventDate: '20',
+//       eventDay: 'MON',
+//       events: [
+//         {
+//           startTime: '10:00 AM',
+//           endTime: '11:00 AM',
+//           date: 'Monday, 19 February 2024',
+//         },
+//         {
+//           startTime: '10:00 AM',
+//           endTime: '11:00 AM',
+//           date: 'Monday, 19 February 2024',
+//         },
+//         {
+//           startTime: '10:00 AM',
+//           endTime: '11:00 AM',
+//           date: 'Monday, 19 February 2024',
+//         },
+//         {
+//           startTime: '10:00 AM',
+//           endTime: '11:00 AM',
+//           date: 'Monday, 19 February 2024',
+//         },
+//         {
+//           startTime: '10:00 AM',
+//           endTime: '11:00 AM',
+//           date: 'Monday, 19 February 2024',
+//         },
+//       ],
+//     },
+//     {
+//       eventDate: '21',
+//       eventDay: 'MON',
+//       events: [
+//         {
+//           startTime: '10:00 AM',
+//           endTime: '11:00 AM',
+//           date: 'Monday, 19 February 2024',
+//         },
+//       ],
+//     },
+//     {
+//       eventDate: '22',
+//       eventDay: 'MON',
+//       events: [
+//         {
+//           startTime: '10:00 AM',
+//           endTime: '11:00 AM',
+//           date: 'Monday, 19 February 2024',
+//         },
+//       ],
+//     },
+//     {
+//       eventDate: '23',
+//       eventDay: 'MON',
+//       events: [
+//         {
+//           startTime: '10:00 AM',
+//           endTime: '11:00 AM',
+//           date: 'Monday, 19 February 2024',
+//         },
+//       ],
+//     },
+//   ],
+// }
