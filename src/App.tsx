ede305a8d6799f4a7c9946724c89cd11a88c3b3d
Masud Rana <masudrana2779@gmail.com>
2024-02-25 16:50:52 +0600

    added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import DemoComponent from "./components/demo";
+import Toast from "./components/ScheduleListView/tost";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <DemoComponent message={"Welcome my repo"} />
+        <Toast />
       </header>
     </div>
   );
